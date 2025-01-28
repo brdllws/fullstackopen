35f9d8d3df0b41b00bf3fd3bcd0dacdb57e1b8a0
Bradley Lewis <bradley.lewis@mail.utoronto.ca>
2025-01-28 13:42:54 -0500

    3.1-3.6

---
+++ part3/index.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    const datetime = new Date();
+    response.send(`Phonebook has info for ${persons.length} people<br>${datetime}`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(p => p.id === id)
+    if (person) {
+        response.json(person)
+    }
+    else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(p => p.id !== id)
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const newPerson = request.body
+    if (!newPerson.name || !newPerson.number) {
+        return response.status(400).json({ error: 'missing name and/or number' })        
+    }
+    if ((persons.filter(p => p.name === newPerson.name)).length > 0) {
+        return response.status(400).json({ error: 'name already exists' })
+    }
+    newPerson.id = String(Math.floor(Math.random()*100000))
+    persons = persons.concat(newPerson)
+    response.json(newPerson)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
